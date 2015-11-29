22ab229ed194bcaa9ef226026213ad95d52b8f4d
vgibbs <vgibbs@etsy.com>
2015-11-29 14:05:35 -0500

    Added test modes

--- SecretSanta.py
+++ SecretSanta.py
@@ -4,6 +4,8 @@
 # Based on Justin's Secret Santa Program by Justin Michel #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+import sys
+import getopt
 import random
 import smtplib
 import getpass
@@ -19,10 +21,25 @@ fromaddr = "your_secret_santa_email@gmail.com"
 username = "your_secret_santa_email"
 password = "********"
 
+def randomize():
+    count = 0
+    while count < 100000:
+        x = random.randint(0, numNames)
+        y = random.randint(0, numNames)
+        shuffle(x, y)
+        count = count + 1
+
 def shuffle(x, y):
     first = names[x]
     names[x] = names[y]
     names[y] = first
+
+def setupMailServer():
+    password = getpass.getpass()
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    return server
 
 def buildEmail(toAddr, toName, secretName):
     msg = """From: Secret Santa <your_secret_santa_email@gmail.com>
@@ -32,25 +49,37 @@ Subject: Your Secret Santa Assignment!
 Greetings %s! Using a very carefully derived formula (and a little bit of magic), the Secret Santa Program has picked you to be the Secret Santa for %s!!!
 
 Have a nice day!
---Santa's Helper""" % (toAddr, toName, toName, secretName)
+--Santa's Helper""" % (toName, toAddr, toName, secretName)
+    return msg
 
-    return msg;
+def main(argv):
+    mode = 'test'
+    # file = ''
+    try:
+        opts, args = getopt.getopt(argv,"m:f:",["mode=","file="])
+    except getopt.GetoptError:
+        print 'SecretSanta.py [-m <test|longtest|exec>] [-f <file>]'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-m", "--mode"):
+            arg = arg.lower()
+            if arg == 'test' or arg == 'longtest' or arg == 'exec':
+                mode = arg
+            else:
+                print '%s is not a valid mode, please use "test" or "longtest" or "exec"' % (arg)
+                sys.exit(2)
+        # elif opt in ("-f", "--file"):
+            # file = arg
 
-def randomize():
-    count = 0
-    while count < 100000:
-        x = random.randint(0, numNames)
-        y = random.randint(0, numNames)
-        shuffle(x, y)
-        count = count + 1
+    print 'Running in %s mode' % (mode)
+    if mode != 'exec':
+        print 'Use -m exec to run in execute mode'
 
-def main():
+    server = ''
+    if mode == 'exec':
+        server = setupMailServer()
+
     randomize()
-
-    password = getpass.getpass()
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.starttls()
-    server.login(username, password)
     for i in range(len(names)):
         santa = names[i][0]
         santaEmail = names[i][1]
@@ -59,12 +88,20 @@ def main():
             nextIndex = 0
         secret = names[nextIndex][0]
         msg = buildEmail(santaEmail, santa, secret)
-        server.sendmail(fromaddr, santaEmail, msg)
 
-    server.quit()
+        if mode == 'exec':
+            server.sendmail(fromaddr, santaEmail, msg)
+        elif mode == 'longtest':
+            print '------------------------'
+            print msg
+        else:
+            print '%s -> %s' % (santa, secret)
+
+    if mode == 'exec':
+        server.quit()
 
     return 0
 
 if __name__ == '__main__':
-    status = main()
+    status = main(sys.argv[1:])
 
