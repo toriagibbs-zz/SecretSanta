03549085c923cf1cafebce9f1843297b9eb0b54c
Tim Cheeseman <timothy.w.cheeseman@gmail.com>
2015-12-01 18:43:15 -0500

    Update README; load participants from CSV.

--- README.md
+++ README.md
@@ -10,17 +10,44 @@ BONUS: Cat Mode! The script supports two 'types' of Secret Santa:
 
 ## Setup
 
-It's simple! Download (cut and paste or git clone/fork) the SecretSanta.py file and make the following updates:
-* Add your users to the names array
+It's simple! Download (cut and paste or git clone/fork) the SecretSanta.py file and make the following preparations:
+* Create a CSV file (Excel, Google Spreadsheets, etc. can export to CSV format) with your participants information. See examples in the [examples](https://github.com/toriagibbs/SecretSanta/blob/master/examples) directory.
   * Each row has the format [name, email, cats' names, address, notes]
-  * For a 'simple' Secret Santa, only fill out name and email
-* Update the fromAddr, username, password variables for the gmail account which will send the assignments
+  * For a 'simple' Secret Santa, only name and email are required
 * Consider asking a friend to send the emails, as they will show up in the sender's outbox!
 
 ## Usage
-`./SecretSanta.py [-m <test|longtest|exec>] [-t <simple|cats>]`
+```
+usage: SecretSanta.py [-h] [-m {test,longtest,exec}] [-t {simple,cats}]
+                      [-e EMAIL] [-u USERNAME] [-p PASSWORD] [-r {gmail}]
+                      [-d DUE_DATE]
+                      participants_file
 
-* Runs with **mode=test** and **type=simple** by default
+Secret Santa
+
+positional arguments:
+  participants_file     CSV file containing participant info
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -m {test,longtest,exec}, --mode {test,longtest,exec}
+  -t {simple,cats}, --type {simple,cats}
+  -e EMAIL, --email EMAIL
+                        email address to send from
+  -u USERNAME, --username USERNAME
+                        username for email account
+  -p PASSWORD, --password PASSWORD
+                        password for email account
+  -r {gmail}, --provider {gmail}
+                        email provider
+  -d DUE_DATE, --due-date DUE_DATE
+                        due date in YYYY-MM-DD format
+```
+
+The `username` and `password` arguments can be omitted and will be prompted for if necessary.
+For example, `./SecretSanta.py -e secretsanta@gmail.com -d 2015-12-15 -m exec -t cats 2015_secret_santa.csv`
+
+* Runs with **mode=test**, **type=simple**, and **provider=gmail** by default
 * Trouble authenticating with Google SMTP? Try [enabling less secure apps](https://www.google.com/settings/u/1/security/lesssecureapps).
 
 | Mode     | Description                                                |
@@ -33,5 +60,4 @@ It's simple! Download (cut and paste or git clone/fork) the SecretSanta.py file 
 |--------|---------------------------------------------------------------------------------------------------------|
 | simple | Simple Secret Santa assigns by name                                                                     |
 | cats   | Cats Secret Santa assigns by cats and includes owner's name, mailing location, and notes about the cats |
-
 
--- SecretSanta.py
+++ SecretSanta.py
@@ -5,6 +5,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 import argparse
+import csv
 import collections
 import datetime
 import getpass
@@ -12,24 +13,24 @@ import random
 import smtplib
 
 
-class Enum(object):
+class StringEnum(object):
     @classmethod
     def choices(cls):
         return [v for (k, v) in vars(cls).iteritems() if not k.startswith('_')]
 
 
-class Mode(Enum):
+class Mode(StringEnum):
     TEST = 'test'
     LONGTEST = 'longtest'
     EXEC = 'exec'
 
 
-class Type(Enum):
+class Type(StringEnum):
     SIMPLE = 'simple'
     CATS = 'cats'
 
 
-class Provider(Enum):
+class Provider(StringEnum):
     GMAIL = 'gmail'
 
 
@@ -63,12 +64,14 @@ Please mail your gift to {secret_name} at {secret_address} by {due_date}.
 DEFAULT_DUE_DATE = datetime.date.today().replace(month=12, day=15)
 
 
-PARTICIPANTS = [
-        ("Joe", "joe@gmail.com", "Fluffy", "the Brooklyn office", "Fluffy is crazy for catnip!"), \
-        ("Mike", "mike@gmail.com", "Fish and Chips", "the Hudson office", "Fish and Chips are tearing up my furniture, they need a scratching post."), \
-        ("Anne", "anne@gmail.com", "Naga and Momo", "the Brooklyn office", "Naga and Momo love toys with feathers."), \
-        ("Sally", "sally@gmail.com", "Romeow", "123 State Street, Seattle, WA 00123", "Romeow likes toys that dangle from strings.")
-]
+Participant = collections.namedtuple('Participant', ['name', 'email', 'cats', 'address', 'notes'])
+Participant.__new__.__defaults__ = (None, None, None)
+
+
+def read_participants(participants_file):
+    with open(participants_file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        return [Participant(*row) for row in reader]
 
 
 def setup_mail_server(username, password, provider):
@@ -93,19 +96,20 @@ def main():
     parser.add_argument('-m', '--mode', choices=Mode.choices(), default=Mode.TEST)
     parser.add_argument('-t', '--type', choices=Type.choices(), default=Type.SIMPLE)
     parser.add_argument('-e', '--email', type=str, help='email address to send from')
-    parser.add_argument('-u', '--username', type=str, required=False,
-                        help='username for email account')
-    parser.add_argument('-p', '--password', type=str, required=False,
-                        help='password for email account')
-    parser.add_argument('-r', '--provider', choices=Provider.choices(), default=Provider.GMAIL,
-                        help='email provider')
-    parser.add_argument('-d', '--due-date', type=parse_date, default=DEFAULT_DUE_DATE)
+    parser.add_argument('-u', '--username', type=str, help='username for email account')
+    parser.add_argument('-p', '--password', type=str, help='password for email account')
+    parser.add_argument('-r', '--provider', choices=Provider.choices(),
+                        default=Provider.GMAIL, help='email provider')
+    parser.add_argument('-d', '--due-date', type=parse_date, default=DEFAULT_DUE_DATE,
+                        help='due date in YYYY-MM-DD format')
+    parser.add_argument('participants_file', help='CSV file containing participant info')
 
     args = parser.parse_args()
     mode = args.mode
     type = args.type
     email = args.email
     due_date = args.due_date.strftime('%a %b %-d')
+    participants = read_participants(args.participants_file)
 
     print 'Running in %s mode' % (mode)
     if mode != Mode.EXEC:
@@ -118,41 +122,33 @@ def main():
         password = args.password if args.password else getpass.getpass('Password: ')
         server = setup_mail_server(username, password, args.provider)
 
-    participants = PARTICIPANTS # TODO: load from CSV file
     random.shuffle(participants)
 
     for (santa, secret) in pair_up(participants):
-        santa_name = santa[0]
-        santa_email = santa[1]
-        secret_name = secret[0]
-
         if type == Type.CATS:
-            secret_cats = secret[2]
-            secret_address = secret[3]
-            secret_notes = secret[4]
-            cats = CATS_TEMPLATE.format(secret_name=secret_name,
-                                        secret_notes=secret_notes,
-                                        secret_address=secret_address,
+            cats = CATS_TEMPLATE.format(secret_name=secret.name,
+                                        secret_notes=secret.notes,
+                                        secret_address=secret.address,
                                         due_date=due_date)
             msg = EMAIL_TEMPLATE.format(from_addr=email,
-                                        to_name=santa_name,
-                                        to_email=santa_email,
-                                        secret_name=secret_name,
+                                        to_name=santa.name,
+                                        to_email=santa.email,
+                                        secret_name=secret.cats,
                                         cats=cats)
         else:
             msg = EMAIL_TEMPLATE.format(from_addr=email,
-                                        to_name=santa_name,
-                                        to_email=santa_email,
-                                        secret_name=secret_name,
+                                        to_name=santa.name,
+                                        to_email=santa.email,
+                                        secret_name=secret.name,
                                         cats='')
 
         if mode == Mode.EXEC:
-            server.sendmail(email, santa_email, msg)
+            server.sendmail(email, santa.email, msg)
         elif mode == Mode.LONGTEST:
             print '{0:-<30}'.format('')
             print msg
         else:
-            print '{} -> {}'.format(santa_name, secret_name)
+            print '{} -> {}'.format(santa.name, secret.name)
 
     if mode == Mode.EXEC:
         server.quit()
